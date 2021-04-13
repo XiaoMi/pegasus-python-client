09251c0a38880839da7c4abac6514ff7a3d05367
tangyanzhao <tangyanzhao@xiaomi.com>
2021-04-13 15:34:42 +0800

    1

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -20,6 +20,7 @@ def getstatusoutput(cmd):
         data = data[:-1]
     return status, data
 
+
 class ServerOperator(object):
     shell_path = '/home/smilencer/Code/incubator-pegasus'
 
@@ -27,31 +28,31 @@ class ServerOperator(object):
     def modify_conf(cls, old_conf, new_conf):
         origin_conf_file = cls.shell_path + '/src/server/config-server.ini'
         status, output = getstatusoutput('sed -i "s/%s/%s/" %s'
-                                                  % (old_conf, new_conf, origin_conf_file))
+                                         % (old_conf, new_conf, origin_conf_file))
         # print(status, output)
 
     @classmethod
     def start_cluster(cls, meta_count, replica_count, check_health):
         status, output = getstatusoutput('cd %s && ./run.sh start_onebox -m %s -r %s'
-                                                  % (cls.shell_path, meta_count, replica_count))
-        print(status,output)
+                                         % (cls.shell_path, meta_count, replica_count))
+        print(status, output)
         if check_health:
             cls.wait_until_cluster_health()
         else:
-            time.sleep(1)   # wait a while for meta ready
+            time.sleep(1)  # wait a while for meta ready
 
     @classmethod
     def stop_and_clear_cluster(cls):
         status, output = getstatusoutput('cd %s && ./run.sh stop_onebox'
-                                                  % cls.shell_path)
+                                         % cls.shell_path)
 
         status, output = getstatusoutput('cd %s && ./run.sh clear_onebox'
-                                                  % cls.shell_path)
+                                         % cls.shell_path)
 
     @classmethod
     def operate_1_server(cls, op_type, server_type, index):
         status, output = getstatusoutput('cd %s && ./run.sh %s_onebox_instance -%s %s'
-                                                  % (cls.shell_path, op_type, server_type, index))
+                                         % (cls.shell_path, op_type, server_type, index))
         # print(status, output)
 
     @classmethod
@@ -76,11 +77,12 @@ class ServerOperator(object):
 
     @classmethod
     def wait_until_cluster_health(cls):
+        cmd = ('cd %s && echo "app temp -d" | ./run.sh shell |'
+               ' grep fully_healthy_partition_count | awk \'{print $NF}\''
+               % cls.shell_path)
+        print(cmd)
         while True:
-            status, output = getstatusoutput(
-                'cd %s && echo "app temp -d" | ./run.sh shell |'
-                ' grep fully_healthy_partition_count | awk \'{print $NF}\''
-                % cls.shell_path)
+            status, output = getstatusoutput(cmd)
             # 0 means return value, 8 means fully_healthy_partition_count = 8
             if status == 0 and output == '8':
                 break
@@ -126,7 +128,7 @@ class TestIntegration(unittest.TestCase):
             ret = yield self.loop_op()
             if not ret:
                 wait_times += 1
-                time.sleep(2)
+                time.sleep(1)
                 if wait_times >= self.MAX_RETRY_COUNT:
                     self.assertTrue(False)
             else:
@@ -137,7 +139,6 @@ class TestIntegration(unittest.TestCase):
         yield self.init(3, 3)
         (ret, ign) = yield self.c.set(self.TEST_HKEY, self.TEST_SKEY, self.TEST_VALUE)
         self.assertEqual(ret, error_types.ERR_OK.value)
-
 
     @inlineCallbacks
     def test_can_not_connect(self):
@@ -252,12 +253,12 @@ class TestIntegration(unittest.TestCase):
 
         for i in range(2):
             ServerOperator.stop_1_replica(1)
-            ServerOperator.stop_1_meta(i+1)
+            ServerOperator.stop_1_meta(i + 1)
             time.sleep(3)
 
             yield self.check_data()
 
-            ServerOperator.start_1_meta(i+1)
+            ServerOperator.start_1_meta(i + 1)
             ServerOperator.start_1_replica(1)
             time.sleep(3)
 
